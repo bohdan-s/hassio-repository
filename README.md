19540a97898f3b1e77d13b22414cfb20d70c3d52
bohdan-s <bohdan-s@users.noreply.github.com>
2022-12-22 21:24:09 +1100

    v0.1.0 Home Assistant Add-on with Discovery

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ This repository contains Hass.io add-ons. All add-ons in this repository are tes
 
 2. Click the 3-dots menu at upper right <kbd>...</kbd> > <kbd>Repositories</kbd> and add this repository's URL: [https://github.com/bohdan-s/hassio-repository](https://github.com/bohdan-s/hassio-repository)
 
+<img src="img/addrepo_config.png" width=300/>
+<img src="img/addrepo_addons.png" width=400/>
+<img src="img/addrepo_store.png" width=200/>
+<img src="img/addrepo_repositories.png" width=200/>
+<img src="img/addrepo_add.png" width=300/>
+
 3. Scroll down the page to find the new repository, and click the new add-on named you want. Ex:
+
+   <img src="img/addon_select.png" width=300/>
 
 4. Click <kbd>Install</kbd> and give it a few minutes to finish downloading.
 
@@ -21,5 +29,5 @@ Enable `Auto update` on the desired add-on or browse Hassio Add-on Store Tab to 
 # Add-ons in this Repository
 
 ### SunGather
-This addon support Sungrow Solar Inverter to publish data to MQTT Broker. 
+This addon support Sungrow Solar Inverter to publish data to MQTT Broker with AutoDiscovery. 
 This add on based on [SunGather](https://github.com/bohdan-s/SunGather)
--- SunGather/run.sh
+++ SunGather/run.sh
@@ -5,7 +5,7 @@ if [ ! -d /share/SunGather ]; then
 fi
 
 if [ ! -f /share/SunGather/config.yaml ]; then
-    cp config-example.yaml /share/SunGather/config.yaml
+    cp config-hassio.yaml /share/SunGather/config.yaml
 fi
 
 INVERTER_HOST=$(bashio::config 'host')
@@ -13,16 +13,31 @@ INTERVAL=$(bashio::config 'scan_interval')
 CONNECTION=$(bashio::config 'connection')
 SMART_METER=$(bashio::config 'smart_meter')
 LOG_CONSOLE=$(bashio::config 'log_console')
-LEVEL=$(bashio::config 'level')
-WEBUI=$(bashio::config 'webui')
+
+if ! bashio::services.available "mqtt"; then
+   bashio::exit.nok "No internal MQTT Broker found. Please install Mosquitto broker."
+else
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASS=$(bashio::services mqtt "password")
+fi
 
 yq -i "
   .inverter.host = \"$INVERTER_HOST\" |
   .inverter.scan_interval = $INTERVAL |
   .inverter.connection = \"$CONNECTION\" |
   .inverter.smart_meter = $SMART_METER |
-  .inverter.log_console = \"$LOG_CONSOLE\" |
-  .inverter.level = $LEVEL
+  .inverter.log_console = \"$LOG_CONSOLE\"
+" /share/SunGather/config.yaml
+
+yq -i "
+  (.exports[] | select(.name == \"mqtt\") | .enabled) = True |
+  (.exports[] | select(.name == \"mqtt\") | .host) = \"$MQTT_HOST\" |
+  (.exports[] | select(.name == \"mqtt\") | .port) = $MQTT_PORT |
+  (.exports[] | select(.name == \"mqtt\") | .username) = \"$MQTT_USER\" |
+  (.exports[] | select(.name == \"mqtt\") | .password) = \"$MQTT_PASS\" |
+  (.exports[] | select(.name == \"mqtt\") | .homeassistant) = True
 " /share/SunGather/config.yaml
 
 yq -i "
@@ -32,7 +47,8 @@ yq -i "
 " /share/SunGather/config.yaml
 
 yq -i "
-  (.exports[] | select(.name == \"webserver\") | .enabled) = $WEBUI
+  (.exports[] | select(.name == \"webserver\") | .enabled) = True |
+  (.exports[] | select(.name == \"webserver\") | .port) = 8099
 " /share/SunGather/config.yaml
 
 exec python3 /sungather.py -c /share/SunGather/config.yaml -l /share/SunGather/
